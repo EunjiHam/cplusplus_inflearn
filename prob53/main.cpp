7647bc3df1815d705564186af885646a24803443
ejham <ehfdl5252@naver.com>
2022-06-06 20:07:03 +0900

    Fix using STL library stack prob53

--- prob53/main.cpp
+++ prob53/main.cpp
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
-
-int stack[100], top = -1;
-
-void push(int x){
-    stack[++top] = x;
-}
-
-int pop(){
-    return stack[top--];
-}
 
 int main()
 {
@@ -20,16 +11,19 @@ int main()
 
     int n, k;
 
+    stack<int> s;
+
     char str[20] = "0123456789ABCDEF";
     scanf("%d %d", &n, &k);
 
     while (n > 0){
-        push(n % k);
+        s.push(n % k);
         n = n / k;
     }
 
-    while (top != -1){
-        printf("%c", str[pop()]);
+    while (!s.empty()){
+        printf("%c", str[s.top()]);
+        s.pop();
     }
 
     return 0;
