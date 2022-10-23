ab008ec245e8a4961c708c0dcb3861ba4ab54de4
ejham <ehfdl5252@naver.com>
2022-10-23 21:10:22 +0900

    Add cplusplus problem 67

---
+++ prob67/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int map[30][30], ch[30], n, cost = 214700;
+
+void DFS(int v, int sum)
+{
+    int i = 0;
+
+    if (v == n){
+        if (sum < cost){
+            cost = sum;
+        }
+    }
+    else {
+        for (i = 1; i <= n; i++){
+            if (map[v][i] > 0 && ch[i] == 0){
+                ch[i] = 1;
+                DFS(i, sum + map[v][i]);
+                ch[i] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    freopen("input.txt", "rt", stdin);
+
+    int m, i, a, b, c;
+
+    scanf("%d %d", &n, &m);
+
+    for (i = 1; i <= m; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        map[a][b] = c;
+    }
+
+    ch[1] = 1;
+    DFS(1, 0);
+    ch[1] = 0;
+
+    printf("%d\n", cost);
+
+    return 0;
+}
